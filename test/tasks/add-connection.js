3b937e387fcb42c9ba93cd71cd299f78056f6b5c
Alexander Price <alexander@Alexanders-MacBook-Pro.local>
2015-04-13 14:55:10 -0500

    looks to be working, just need tweaking

--- test/tasks/add-connection.js
+++ test/tasks/add-connection.js
@@ -1,10 +1,21 @@
+var sendgrid = require('sendgrid')('app11605236@heroku.com', 'kl0bxcba');
+
 module.exports = {
   options: {
     allowContext: true
   },
   task: {
     execute: function(email, context, callback) {
-      callback(context.company + ' sent an email to ' + email + '.');
+      sendgrid.send({
+        to: email,
+        toname: context.company || '',
+        from: 'support@tourconnect.com',
+        html: 'Connected!',
+        subject: 'Connected!'
+      }, function(error, response) {
+        console.log(error, response);
+        callback(context.company + ' sent an email to ' + email + '.');
+      });
     },
     error: function() {
 
